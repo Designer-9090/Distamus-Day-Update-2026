fd3f794a3ab1e6b5feca97a826ec6a83c87d3849
Distamus <dma04018@gmail.com>
2026-02-28 23:32:40 +0100

    Simplify paths in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        ddu2025: resolve(__dirname, 'ddu2025.html'),
+        main: 'index.html',
+        ddu2025: 'ddu2025.html',
       },
     },
   },
