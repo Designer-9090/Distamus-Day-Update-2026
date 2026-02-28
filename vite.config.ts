611937539105794e23f816f345afb70b6df3e79c
Distamus <dma04018@gmail.com>
2026-02-28 23:30:24 +0100

    Add custom build configuration for Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        ddu2025: resolve(__dirname, 'ddu2025.html'),
+      },
+    },
+  },
 })
